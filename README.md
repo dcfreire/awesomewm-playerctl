4324fcc70995115e98834ca441d73f9e4856bd63
Daniel Freire <daniel.carneiro.freire@gmail.com>
2022-04-10 18:24:01 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Playerctl widget for AweomseWM
+
+Widget to control whatever music player you're using with [playerctl](https://github.com/altdesktop/playerctl). Based on [this widget](https://github.com/streetturtle/awesome-wm-widgets/blob/master/spotify-widget/spotify.lua) by streetturtle.
+
+## Installation
+
+Clone this repo under `~/.config/awesome/widgets/`, then add it to your `theme.lua`.
+
+```lua
+playerctl_widget{layer="player_name"} --Defaults to "spotify"
+```
+
+## Customization
+
+It is possible to customize widget by providing a table with all or some of the following config parameters:
+| Name | Default | Description |
+|---|---|---|
+| `player` | `"spotify"` | Player |
+| `play_icon` | `"~/.config/awesome/widgets/playerctl/player_play.png"` | Play icon |
+| `pause_icon` | `"~/.config/awesome/widgets/playerctl/player_pause.png"` | Pause icon |
+| `font` | `"Play 9"`| Font |
+| `dim_when_paused` | `false` | Decrease the widget opacity if spotify is paused |
+| `dim_opacity` | `0.2` | Widget's opacity when dimmed, `dim_when_paused` should be set to `true` |
+| `max_length` | `15` | Maximum lentgh of artist and title names. Text will be ellipsized if longer. |
+| `show_tooltip` | `true` | Show tooltip on hover with information about the playing song |
+| `timeout` | `1` | How often in seconds the widget refreshes |
+| `bg` | `#ffcb60` | Tooltip background |
+| `fg` | `#000000` | Tooltip foreground |
+
+## Dependencies
+
+This widget depends on [playerctl](https://github.com/altdesktop/playerctl).
---
+++ playerctl.lua
@@ -0,0 +1,167 @@
+-------------------------------------------------
+-- Playerctl Widget for Awesome Window Manager
+-- Requires playerctl
+-------------------------------------------------
+
+local awful = require("awful")
+local wibox = require("wibox")
+local watch = require("awful.widget.watch")
+
+
+
+
+local function ellipsize(text, length)
+    return (text:len() > length and length > 0)
+        and text:sub(0, length - 3) .. '...'
+        or text
+end
+
+local widget = {}
+
+local function worker(args)
+
+    local args = args or {}
+
+    local player = args.player or "spotify"
+    local command = 'playerctl -p ' .. player .. ' '
+    local GET_STATUS_CMD = command .. ' status'
+    local GET_CURRENT_SONG_CMD = command ..' metadata'
+
+    local play_icon = args.play_icon or os.getenv("HOME") .. '/.config/awesome/widgets/playerctl/player_play.png'
+    local pause_icon = args.pause_icon or os.getenv("HOME") .. '/.config/awesome/widgets/playerctl/player_pause.png'
+    local font = args.font or 'Play 9'
+    local dim_when_paused = args.dim_when_paused == nil and false or args.dim_when_paused
+    local dim_opacity = args.dim_opacity or 0.2
+    local max_length = args.max_length or 15
+    local show_tooltip = args.show_tooltip == nil and true or args.show_tooltip
+    local timeout = args.timeout or 1
+
+    local cur_artist = ''
+    local cur_title = ''
+    local cur_album = ''
+    local status = false
+
+    widget = wibox.widget {
+        {
+            id = 'artistw',
+            font = font,
+            widget = wibox.widget.textbox,
+        },
+        {
+            id = "icon",
+            widget = wibox.widget.imagebox,
+        },
+        {
+            layout = wibox.container.scroll.horizontal,
+            max_size = 100,
+            step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
+            speed = 40,
+            {
+                id = 'titlew',
+                font = font,
+                widget = wibox.widget.textbox
+            }
+        },
+        layout = wibox.layout.align.horizontal,
+        set_status = function(self, is_playing)
+            self.icon.image = (is_playing and play_icon or pause_icon)
+            status = is_playing
+            if dim_when_paused then
+                self:get_children_by_id('icon')[1]:set_opacity(is_playing and 1 or dim_opacity)
+
+                self:get_children_by_id('titlew')[1]:set_opacity(is_playing and 1 or dim_opacity)
+                self:get_children_by_id('titlew')[1]:emit_signal('widget::redraw_needed')
+
+                self:get_children_by_id('artistw')[1]:set_opacity(is_playing and 1 or dim_opacity)
+                self:get_children_by_id('artistw')[1]:emit_signal('widget::redraw_needed')
+            end
+        end,
+
+
+        set_text = function(self, artist, song)
+            local artist_to_display = ellipsize(artist, max_length)
+            if self:get_children_by_id('artistw')[1]:get_markup() ~= artist_to_display then
+                self:get_children_by_id('artistw')[1]:set_markup(artist_to_display)
+            end
+            local title_to_display = ellipsize(song, max_length)
+            if self:get_children_by_id('titlew')[1]:get_markup() ~= title_to_display then
+                self:get_children_by_id('titlew')[1]:set_markup(title_to_display)
+            end
+        end
+    }
+
+    local update_widget_icon = function(widget, stdout, _, _, _)
+        stdout = string.gsub(stdout, "\n", "")
+        widget:set_status(stdout == 'Playing' and true or false)
+    end
+
+    local update_widget_text = function(widget, stdout, _, _, _)
+        if string.len(stdout) == 0 then
+            widget:set_text('','')
+            widget:set_visible(false)
+            return
+        end
+
+        local escaped = string.gsub(stdout, "&", '&amp;')
+        local album, _album_artist, artist, title =
+            string.match(escaped, 'spotify xesam:album%s*([^\n]*)\n.*spotify xesam:albumArtist%s*([^\n]*)\n.*spotify xesam:artist%s*([^\n]*)\n.*spotify xesam:title%s*([^\n]*)')
+        if album ~= nil and title ~=nil and artist ~= nil then
+            cur_artist = artist
+            cur_title = title
+            cur_album = album
+
+            widget:set_text(artist, title)
+            widget:set_visible(true)
+        end
+    end
+
+    watch(GET_STATUS_CMD, timeout, update_widget_icon, widget)
+    watch(GET_CURRENT_SONG_CMD, timeout, update_widget_text, widget)
+
+    --- Adds mouse controls to the widget:
+    --  - left click - play/pause
+    --  - scroll up - play next song
+    --  - scroll down - play previous song
+    widget:connect_signal("button::press", function(_, _, _, button)
+        print(button)
+        if (button == 1) then
+            awful.spawn(command .. (status and "pause" or "play"), false)      -- left click
+        elseif (button == 4) then
+            awful.spawn(command .. "next", false)  -- scroll up
+        elseif (button == 5) then
+            awful.spawn(command .. "prev", false)  -- scroll down
+        elseif (button == 2) then
+            awful.spawn("killall " .. player, false)  -- middle mouse
+        end
+        awful.spawn.easy_async(GET_CURRENT_SONG_CMD, function(stdout, stderr, exitreason, exitcode)
+            update_widget_icon(widget, stdout, stderr, exitreason, exitcode)
+        end)
+    end)
+
+
+    if show_tooltip then
+        local playerctl_tooltip = awful.tooltip {
+            mode = 'outside',
+            preferred_positions = {'bottom'},
+            bg = args.bg or "#ffcb60",
+            fg = args.fg or "#000000",
+            font = font
+         }
+
+        playerctl_tooltip:add_to_object(widget)
+
+        widget:connect_signal('mouse::enter', function()
+            playerctl_tooltip.markup = '<b>Album</b>: ' .. cur_album
+                .. '\n<b>Artist</b>: ' .. cur_artist
+                .. '\n<b>Song</b>: ' .. cur_title
+        end)
+    end
+
+    return widget
+
+end
+
+return setmetatable(widget, { __call = function(_, ...)
+    return worker(...)
+end
+})
